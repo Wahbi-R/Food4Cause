4176bde34f97395899d15b0ada250271992c88d8
Wahbi <wahbi.raihan@ontariotechu.net>
2021-11-01 00:53:25 -0400

    Finished homepage & basic About Us

---
+++ food4cause/lib/about.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AboutUs extends StatelessWidget {
+  const AboutUs({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Padding(
+                padding: new EdgeInsets.only(top: 60),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text("About us\n",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    Text('''This is our about page. Here we will
+outline our goals and reasons for this
+project. We plan to include some tabs;
+who we are, financial accountability, etc
+As well, a graphical mock timeline.\n'''),
+                    Text('''For this mockup, we don't have much
+done about our about page but it will 
+look better formatted and more user 
+friendly by the time of our final project
+submission date.\n'''),
+                    Text(
+                        "This page will have a dark blue theme in\nits widget/photo layouts"),
+                  ],
+                ))));
+  }
+}
---
+++ food4cause/lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'about.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food4Cause',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Food4Cause'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        //Icon and buttons on main page
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Icon(Icons.local_restaurant, size: 60),
+          const SizedBox(height: 20),
+          Text("Food4Cause",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold, color: Colors.grey[700])),
+          const SizedBox(height: 20),
+          elevButtons("Donate", Colors.red[400], null, context),
+          const SizedBox(height: 30),
+          elevButtons("Food Partner", Colors.orangeAccent[700], null, context),
+          const SizedBox(height: 30),
+          elevButtons("Community Partner", Colors.yellow[500], null, context),
+          const SizedBox(height: 30),
+          elevButtons("Volunteer", Colors.green[300], null, context),
+          const SizedBox(height: 30),
+          elevButtons("Charity Partner", Colors.blue[300], null, context),
+          const SizedBox(height: 30),
+          elevButtons("About Us", Colors.blue[900], AboutUs, context),
+          const SizedBox(height: 30),
+        ],
+      ),
+    ));
+  }
+}
+
+void openPage(page, context) {
+  if (page == null) {
+    return;
+  }
+  Navigator.push(context, MaterialPageRoute(builder: (context) => AboutUs()));
+}
+
+Widget elevButtons(String text, color, pageName, context) => Container(
+    width: 300,
+    height: 40,
+    decoration:
+        BoxDecoration(borderRadius: BorderRadius.circular(5), boxShadow: const [
+      BoxShadow(
+          color: Colors.grey,
+          offset: Offset(-3, 3),
+          spreadRadius: 1,
+          blurRadius: 2)
+    ]),
+    child: ElevatedButton(
+        onPressed: () {
+          openPage(pageName, context);
+        },
+        child: Text(text),
+        style: ElevatedButton.styleFrom(primary: color, elevation: 0)));
